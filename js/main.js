9101c451bd39f39a6a004e804d97ac7a2d4876d3
Trent Rosane <trosane@uw.edu>
2015-10-28 10:15:51 -0700

    day one

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 // Create an angular application called "sortApp"
-
+var sortApp = angular.module('sortApp', []);
 
 // Define a controller "mainController" for you application, 
 // making sure to pass in both the $scope and $http variables
+app.controller('mainController', function($scope, $http) {
+    $http.get('data/salaries.json').then(function(response) {
+        $scope.data = response.data;
+    });
+
+});
+
 
 	// Use and $http.get request to get the salary data
-	
 		// Set $scope.data equal to the data returned in your results
